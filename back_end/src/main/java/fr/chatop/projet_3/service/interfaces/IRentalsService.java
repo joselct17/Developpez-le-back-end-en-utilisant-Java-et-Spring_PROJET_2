9a36af0707bf6a389c0dae2e50e144f62cdff468
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:16:24 +0200

    Add CRUD methods to IRentalsService interface
    Introduce methods for creating, reading, updating, and deleting rentals in the IRentalsService interface. This includes methods for retrieving all rentals, getting a rental by its ID, creating a new rental, deleting a rental by ID, and updating a rental using a RentalsDto.

--- back_end/src/main/java/fr/chatop/projet_3/service/interfaces/IRentalsService.java
+++ back_end/src/main/java/fr/chatop/projet_3/service/interfaces/IRentalsService.java
@@ -1,4 +1,21 @@
 package fr.chatop.projet_3.service.interfaces;
 
+import fr.chatop.projet_3.model.Rentals;
+import fr.chatop.projet_3.model.dto.RentalsDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IRentalsService {
+
+  List<Rentals> getAllRentals();
+
+  Optional<Rentals> getRentalById(Integer id);
+
+  Rentals createRental(Rentals rental);
+
+  void deleteRental(Integer id);
+
+  Rentals updateRental(Integer id, RentalsDto rentalDto);
+
 }
