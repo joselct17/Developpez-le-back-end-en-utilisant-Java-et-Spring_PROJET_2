74aaee8cdb587cb06906f919b4d998943169b512
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:49:23 +0200

    Refactor application configuration
    Remove manual auto-configuration exclusions by using @SpringBootApplication. This change simplifies the configuration and relies on Spring Boot's default behavior.

--- back_end/src/main/java/fr/chatop/projet_3/DemoApplication.java
+++ back_end/src/main/java/fr/chatop/projet_3/DemoApplication.java
@@ -1,14 +1,9 @@
 package fr.chatop.projet_3;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-@EnableAutoConfiguration(exclude = {
-  SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
