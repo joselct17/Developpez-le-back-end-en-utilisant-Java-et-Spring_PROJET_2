b02e686b1d0265285623d61ad2922471bca0db80
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:16:06 +0200

    Add CRUD operations to MessageController
    Implemented endpoints for creating, reading, updating, and deleting messages in the MessageController. Added appropriate response statuses for each operation to handle success and error cases. Integrated the IMessageService for service layer interactions.

--- back_end/src/main/java/fr/chatop/projet_3/controller/MessageController.java
+++ back_end/src/main/java/fr/chatop/projet_3/controller/MessageController.java
@@ -1,8 +1,63 @@
 package fr.chatop.projet_3.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import fr.chatop.projet_3.model.Message;
+import fr.chatop.projet_3.model.dto.MessageDto;
+import fr.chatop.projet_3.service.interfaces.IMessageService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("api/messages")
 public class MessageController {
+
+  private final IMessageService messageService;
+
+
+  @GetMapping
+  public ResponseEntity<List<Message>> getAllMessages() {
+    List<Message> messages = messageService.getAllMessages();
+    return ResponseEntity.ok(messages);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Message> getMessageById(@PathVariable Integer id) {
+    Optional<Message> messageOpt = messageService.getMessageById(id);
+    if (messageOpt.isPresent()) {
+      return ResponseEntity.ok(messageOpt.get());
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+
+  @PostMapping
+  public ResponseEntity<Message> createMessage(@RequestBody MessageDto messageDto, @RequestParam Integer userId, @RequestParam Integer rentalId) {
+    Message createdMessage = messageService.createMessage(messageDto, userId, rentalId);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdMessage);
+  }
+
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Message> updateMessage(@PathVariable Integer id, @RequestBody MessageDto messageDto) {
+    Message updatedMessage = messageService.updateMessage(id, messageDto);
+    if (updatedMessage != null) {
+      return ResponseEntity.ok(updatedMessage);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteMessage(@PathVariable Integer id) {
+    messageService.deleteMessage(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
