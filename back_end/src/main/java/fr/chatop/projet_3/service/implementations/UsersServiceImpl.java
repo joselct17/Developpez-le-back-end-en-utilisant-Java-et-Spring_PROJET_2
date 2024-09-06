dbdfdcdc5afad3460bc982e5e183f384d7afaf44
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:14:59 +0200

    Add CRUD functionality to UsersServiceImpl
    Implemented methods to create, read, update, and delete users within the UsersServiceImpl class. Introduced dependency injection for IUserRepository and added relevant model and DTO imports. These changes facilitate comprehensive user management in the application.

--- back_end/src/main/java/fr/chatop/projet_3/service/implementations/UsersServiceImpl.java
+++ back_end/src/main/java/fr/chatop/projet_3/service/implementations/UsersServiceImpl.java
@@ -1,9 +1,59 @@
 package fr.chatop.projet_3.service.implementations;
 
+import fr.chatop.projet_3.model.Users;
+import fr.chatop.projet_3.model.dto.RegisterUserDto;
+import fr.chatop.projet_3.repository.IUserRepository;
 import fr.chatop.projet_3.service.interfaces.IUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
+@AllArgsConstructor
 public class UsersServiceImpl implements IUserService {
+
+  private final IUserRepository userRepository;
+
+  @Override
+  public Users createUser(Users user) {
+    user.setCreatedAt(LocalDateTime.now());
+    return userRepository.save(user);
+  }
+
+  public List<Users> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  @Override
+  public Optional<Users> getUserById(int id) {
+    return userRepository.findById(id);
+  }
+
+  @Override
+  public Users updateUser(Integer id, RegisterUserDto registerUserDto) {
+    Optional<Users> existingUserOpt = userRepository.findById(id);
+    if (existingUserOpt.isPresent()) {
+      Users existingUser = existingUserOpt.get();
+      existingUser.setName(registerUserDto.getName());
+      existingUser.setEmail(registerUserDto.getEmail());
+      existingUser.setPassword(registerUserDto.getPassword());
+      existingUser.setUpdatedAt(LocalDateTime.now());
+      return userRepository.save(existingUser);
+    }
+    return null;
+  }
+
+  @Override
+  public void deleteUser(Integer id) {
+    userRepository.deleteById(id);
+  }
+
+  @Override
+  public Users getUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
 }
