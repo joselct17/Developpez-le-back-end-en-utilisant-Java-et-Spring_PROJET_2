d95730401c220762239ac42b670961cc615a1115
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:15:43 +0200

    Add CRUD endpoints to RentalsController
    Implemented endpoints for creating, reading, updating, and deleting rentals. Utilized IRentalsService to manage rental data and handle responses with appropriate HTTP statuses.

--- back_end/src/main/java/fr/chatop/projet_3/controller/RentalsController.java
+++ back_end/src/main/java/fr/chatop/projet_3/controller/RentalsController.java
@@ -1,8 +1,61 @@
 package fr.chatop.projet_3.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import fr.chatop.projet_3.model.Rentals;
+import fr.chatop.projet_3.model.dto.RentalsDto;
+import fr.chatop.projet_3.service.interfaces.IRentalsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("api/rentals")
 public class RentalsController {
+
+  private final IRentalsService rentalService;
+
+  @GetMapping
+  public ResponseEntity<List<Rentals>> getAllRentals() {
+    List<Rentals> rentals = rentalService.getAllRentals();
+    return ResponseEntity.ok(rentals);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Rentals> getRentalById(@PathVariable Integer id) {
+    Optional<Rentals> rentalOpt = rentalService.getRentalById(id);
+    if (rentalOpt.isPresent()) {
+      return ResponseEntity.ok(rentalOpt.get());
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<Rentals> createRental(@RequestBody Rentals rental) {
+    Rentals createdRental = rentalService.createRental(rental);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdRental);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Rentals> updateRental(@PathVariable Integer id, @RequestBody RentalsDto rentalDto) {
+    Rentals updatedRental = rentalService.updateRental(id, rentalDto);
+    if (updatedRental != null) {
+      return ResponseEntity.ok(updatedRental);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteRental(@PathVariable Integer id) {
+    rentalService.deleteRental(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
