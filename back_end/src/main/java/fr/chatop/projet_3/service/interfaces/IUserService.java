860115663e74911abc7264de87bcfc47c0fd34fd
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:16:14 +0200

    Add CRUD operations to IUserService interface
    Implemented methods for user creation, retrieval, update, and deletion. Included additional methods to get all users and retrieve a user by email. This enhances the interface to support full user management capabilities.

--- back_end/src/main/java/fr/chatop/projet_3/service/interfaces/IUserService.java
+++ back_end/src/main/java/fr/chatop/projet_3/service/interfaces/IUserService.java
@@ -1,4 +1,22 @@
 package fr.chatop.projet_3.service.interfaces;
 
-public interface IUserService {
+import fr.chatop.projet_3.model.Rentals;
+import fr.chatop.projet_3.model.Users;
+import fr.chatop.projet_3.model.dto.RegisterUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+ public interface IUserService {
+   Users createUser(Users user);
+
+   List<Users> getAllUsers();
+
+   Optional<Users> getUserById(int id);
+
+   Users updateUser(Integer id, RegisterUserDto registerUserDto);
+
+   void deleteUser(Integer id);
+
+   Users getUserByEmail(String email);
 }
