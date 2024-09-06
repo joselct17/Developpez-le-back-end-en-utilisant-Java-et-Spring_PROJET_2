7e78cf1336ccbf8eda3cd98689ca8bada09e1cdf
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:16:19 +0200

    Add findByEmail method to IUserRepository
    Enhanced IUserRepository to include a method for finding users by email. This addition allows for more efficient querying and retrieval of user records based on their email addresses.

--- back_end/src/main/java/fr/chatop/projet_3/repository/IUserRepository.java
+++ back_end/src/main/java/fr/chatop/projet_3/repository/IUserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepository extends JpaRepositoryImplementation<Users, Integer> {
+  public Users findByEmail(String email);
 }
