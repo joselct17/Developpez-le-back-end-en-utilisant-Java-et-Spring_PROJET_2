1f1461c4b86eeccff1d384aef571cb3912654d48
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-06 16:15:18 +0200

    Add CRUD operations in UsersController
    Implemented endpoints for user creation, retrieval, updating, and deletion within UsersController. Integrated dependency injection for IUserService and used ResponseEntity for appropriate HTTP responses.

--- back_end/src/main/java/fr/chatop/projet_3/controller/UsersController.java
+++ back_end/src/main/java/fr/chatop/projet_3/controller/UsersController.java
@@ -1,8 +1,61 @@
 package fr.chatop.projet_3.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import fr.chatop.projet_3.model.Users;
+import fr.chatop.projet_3.model.dto.RegisterUserDto;
+import fr.chatop.projet_3.service.interfaces.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
 public class UsersController {
+
+  private final IUserService usersService;
+
+  @GetMapping
+  public ResponseEntity<List<Users>> getAllUsers() {
+    List<Users> users = usersService.getAllUsers();
+    return ResponseEntity.ok(users);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Users> getUserById(@PathVariable Integer id) {
+    Optional<Users> userOpt = usersService.getUserById(id);
+    if (userOpt.isPresent()) {
+      return ResponseEntity.ok(userOpt.get());
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+
+  @PostMapping("/register")
+  public ResponseEntity<Users> createUser(Users users) {
+    Users createdUser = usersService.createUser(users);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Users> updateUser(@PathVariable Integer id, @RequestBody RegisterUserDto registerUserDto) {
+    Users updatedUser = usersService.updateUser(id, registerUserDto);
+    if (updatedUser != null) {
+      return ResponseEntity.ok(updatedUser);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+    usersService.deleteUser(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
