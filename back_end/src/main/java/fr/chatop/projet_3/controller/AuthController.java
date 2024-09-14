cd82debbc0420e8b4c77af5570e9b1e69eeaab0d
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-14 10:54:16 +0200

    Add AuthController for user authentication endpoints
    Implemented AuthController to handle user authentication tasks including login, registration, and retrieval of current

---
+++ back_end/src/main/java/fr/chatop/projet_3/controller/AuthController.java
@@ -0,0 +1,44 @@
+package fr.chatop.projet_3.controller;
+
+
+import fr.chatop.projet_3.config.JWTService;
+import fr.chatop.projet_3.model.Users;
+import fr.chatop.projet_3.service.interfaces.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+  private final JWTService jwtService;
+  private final IUserService usersService;
+
+
+  @PostMapping("/login")
+  public String getToken(Authentication authentication) {
+    String token = jwtService.generateToken(authentication);
+    return token;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<Users> createUser(@RequestBody Users users) {
+
+    Users createdUser = usersService.createUser(users);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<Users> getCurrentUser() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      String email = authentication.getName();
+      Users currentUser = usersService.getUserByEmail(email);
+      return ResponseEntity.ok(currentUser);
+  }
+
+}
