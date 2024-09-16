4a385222ec10a492b3a708c598746829e7e5133c
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-16 18:57:56 +0200

    Add userId and rentalId fields to MessageDto
    This update includes additional fields, userId and rentalId, in the MessageDto class to better encapsulate message-related information. The constructor has been modified to initialize these new fields using JSON properties, ensuring seamless data binding with Jackson.

--- back_end/src/main/java/fr/chatop/projet_3/model/dto/MessageDto.java
+++ back_end/src/main/java/fr/chatop/projet_3/model/dto/MessageDto.java
@@ -1,13 +1,23 @@
 package fr.chatop.projet_3.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.chatop.projet_3.model.Message;
 import lombok.Data;
 
 @Data
 public class MessageDto {
   private String message;
+  private Integer userId;
+  private Integer rentalId;
 
-  public MessageDto(Message message) {
-    this.message = message.getMessage();
+  @JsonCreator
+  public MessageDto(@JsonProperty("message") String message,
+                    @JsonProperty("user_id") Integer userId,
+                    @JsonProperty("rental_id") Integer rentalId) {
+    this.message = message;
+    this.userId = userId;
+    this.rentalId = rentalId;
   }
+
 }
