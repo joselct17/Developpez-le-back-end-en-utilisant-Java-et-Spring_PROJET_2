06b19902727dc0c7f1437be7fac9fe6d11af62cb
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-20 18:33:03 +0200

    Add method to find user by rental ID in IUserRepository
    This commit introduces a custom query method `findOwnerByRentalId` to the `IUserRepository`. This method allows retrieval of a user who owns a rental based on a given rental ID, enhancing the repository's querying capabilities.

--- back_end/src/main/java/fr/chatop/projet_3/repository/IUserRepository.java
+++ back_end/src/main/java/fr/chatop/projet_3/repository/IUserRepository.java
@@ -1,10 +1,16 @@
 package fr.chatop.projet_3.repository;
 
 import fr.chatop.projet_3.model.Users;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepository extends JpaRepositoryImplementation<Users, Integer> {
   public Users findByEmail(String email);
+
+  // Requête personnalisée pour trouver l'utilisateur qui possède une location avec un certain rentalId
+  @Query("SELECT u FROM Users u JOIN u.rentals r WHERE r.id = :rentalId")
+  Users findOwnerByRentalId(@Param("rentalId") Integer rentalId);
 }
