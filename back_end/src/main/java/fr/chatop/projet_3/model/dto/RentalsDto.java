db2cbe8522e8ef3854dc3dd2e69e7a8adc410374
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-20 15:05:28 +0200

    Add id, created_at, and updated_at fields to RentalsDto
    Extended RentalsDto class to include id, created_at, and updated_at fields to better represent rental data. These new fields are also initialized in the constructor using values from the Rentals object.

--- back_end/src/main/java/fr/chatop/projet_3/model/dto/RentalsDto.java
+++ back_end/src/main/java/fr/chatop/projet_3/model/dto/RentalsDto.java
@@ -4,21 +4,28 @@ import fr.chatop.projet_3.model.Rentals;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class RentalsDto {
-
+  private Integer id;
   private String name;
   private BigDecimal surface;
   private BigDecimal price;
   private String picture;
   private String description;
+  private LocalDate created_at;
+  private LocalDate updated_at;
 
   public RentalsDto(Rentals rentals) {
+    this.id = rentals.getId();
     this.name = rentals.getName();
       this.surface = rentals.getSurface();
       this.price = rentals.getPrice();
       this.picture = rentals.getPicture();
       this.description = rentals.getDescription();
+      this.created_at = rentals.getCreatedAt();
+      this.updated_at = rentals.getUpdatedAt();
   }
 }
