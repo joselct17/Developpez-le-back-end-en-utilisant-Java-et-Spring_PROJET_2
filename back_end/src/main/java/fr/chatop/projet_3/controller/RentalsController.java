b44a235394212010dca130283ce99ded10e6c6d4
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-20 15:05:23 +0200

    Add OpenAPI annotations and refactor RentalsController methods
    Enhanced the RentalsController with OpenAPI annotations for better API documentation. Refactored methods to use RentalsDto for responses, ensure proper conversion from Rentals entities, and manage multipart image uploads more effectively.

--- back_end/src/main/java/fr/chatop/projet_3/controller/RentalsController.java
+++ back_end/src/main/java/fr/chatop/projet_3/controller/RentalsController.java
@@ -6,6 +6,11 @@ import fr.chatop.projet_3.model.Users;
 import fr.chatop.projet_3.model.dto.RentalsDto;
 import fr.chatop.projet_3.service.interfaces.IRentalsService;
 import fr.chatop.projet_3.service.interfaces.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,51 +36,82 @@ public class RentalsController {
   private final IRentalsService rentalService;
   private final IUserService usersService;
 
+  /**
+   * Récupère la liste de toutes les locations
+   */
+  @Operation(summary = "Récupérer toutes les locations")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Liste des locations récupérée",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Rentals.class)) })
+  })
   @GetMapping
-  public ResponseEntity<Map<String, List<Rentals>>> getAllRentals() {
-    List<Rentals> rentals = rentalService.getAllRentals(); // Récupère la liste des locations
-    Map<String, List<Rentals>> response = new HashMap<>(); // Crée une nouvelle carte
-    response.put("rentals", rentals); // Ajoute la liste des locations avec la clé "Rentals"
-    return ResponseEntity.ok(response); // Retourne la carte comme réponse HTTP
+  public ResponseEntity<Map<String, List<RentalsDto>>> getAllRentals() {
+    List<Rentals> rentals = rentalService.getAllRentals();
+    // Convertir chaque entité Rentals en RentalsDto
+    List<RentalsDto> rentalsDtoList = rentals.stream()
+      .map(RentalsDto::new)
+      .toList();
+    Map<String, List<RentalsDto>> response = new HashMap<>();
+    response.put("rentals", rentalsDtoList);
+
+    return ResponseEntity.ok(response);
   }
 
-
+  /**
+   * Récupère une location par son identifiant
+   */
+  @Operation(summary = "Récupérer une location par ID")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Location trouvée",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Rentals.class)) }),
+    @ApiResponse(responseCode = "404", description = "Location non trouvée")
+  })
   @GetMapping("/{id}")
-  public ResponseEntity<Rentals> getRentalById(@PathVariable Integer id) {
+  public ResponseEntity<RentalsDto> getRentalById(@PathVariable Integer id) {
     Optional<Rentals> rentalOpt = rentalService.getRentalById(id);
     if (rentalOpt.isPresent()) {
-      return ResponseEntity.ok(rentalOpt.get());
+      RentalsDto rentalDto = new RentalsDto(rentalOpt.get());
+      return ResponseEntity.ok(rentalDto);
     } else {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
   }
 
+
+  /**
+   * Crée une nouvelle location
+   */
+  @Operation(summary = "Créer une nouvelle location")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Location créée avec succès",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Rentals.class)) }),
+    @ApiResponse(responseCode = "400", description = "Erreur dans les données envoyées")
+  })
   @PostMapping
-  public ResponseEntity<Rentals> createRental(
+  public ResponseEntity<RentalsDto> createRental(
     @RequestParam("name") String name,
     @RequestParam("surface") BigDecimal surface,
     @RequestParam("price") BigDecimal price,
     @RequestParam("description") String description,
     @RequestParam("picture") MultipartFile picture) throws IOException {
 
-    // Créer une nouvelle instance de Rentals
     Rentals rental = new Rentals();
     rental.setName(name);
     rental.setSurface(surface);
     rental.setPrice(price);
     rental.setDescription(description);
 
-    // Récupération de l'utilisateur courant
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     String email = authentication.getName();
     Users currentUser = usersService.getUserByEmail(email);
 
-    // Ajouter la location à la liste des locations de l'utilisateur
     List<Rentals> rentals = currentUser.getRentals();
     rentals.add(rental);
     currentUser.setRentals(rentals);
 
-    // Sauvegarder le fichier de l'image si non vide
     if (!picture.isEmpty()) {
       String fileName = UUID.randomUUID().toString() + "_" + picture.getOriginalFilename();
       String uploadDir = "uploads";
@@ -89,31 +125,77 @@ public class RentalsController {
       Files.copy(picture.getInputStream(), filePath);
       rental.setPicture(filePath.toString());
     } else {
-      throw new RuntimeException("Picture file is empty");
+      throw new RuntimeException("Le fichier d'image est vide");
     }
 
-    // Sauvegarder la location et mettre à jour l'utilisateur
     rentalService.createRental(rental);
-    usersService.updateRentalsUser(currentUser); // Met à jour l'utilisateur avec la nouvelle location
+    usersService.updateRentalsUser(currentUser);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(rental);
+    // Retourner un RentalsDto après la création
+    RentalsDto rentalDto = new RentalsDto(rental);
+    return ResponseEntity.status(HttpStatus.CREATED).body(rentalDto);
   }
 
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Rentals> updateRental(@PathVariable Integer id, @RequestBody RentalsDto rentalDto) {
-    Rentals updatedRental = rentalService.updateRental(id, rentalDto);
-    if (updatedRental != null) {
-      return ResponseEntity.ok(updatedRental);
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+  @PutMapping(value = "/{id}", consumes = {"multipart/form-data"})
+  public ResponseEntity<RentalsDto> updateRental(
+    @PathVariable Integer id,
+    @RequestParam("name") String name,
+    @RequestParam("surface") BigDecimal surface,
+    @RequestParam("price") BigDecimal price,
+    @RequestParam("description") String description,
+    @RequestParam(value = "picture", required = false) MultipartFile picture) throws IOException {
+
+    // Récupérer la location à partir de l'ID
+    Rentals rental = rentalService.getRentalById(id)
+      .orElseThrow(() -> new RuntimeException("Rental not found"));
+
+    // Mettre à jour les informations du Rentals avec les nouvelles valeurs
+    rental.setName(name);
+    rental.setSurface(surface);
+    rental.setPrice(price);
+    rental.setDescription(description);
+
+    // Si une image est fournie, mettre à jour le fichier image
+    if (picture != null && !picture.isEmpty()) {
+      String fileName = UUID.randomUUID().toString() + "_" + picture.getOriginalFilename();
+      String uploadDir = "uploads";
+      Path uploadPath = Paths.get(uploadDir);
+
+      if (!Files.exists(uploadPath)) {
+        Files.createDirectories(uploadPath);
+      }
+
+      Path filePath = uploadPath.resolve(fileName);
+      Files.copy(picture.getInputStream(), filePath);
+      rental.setPicture(filePath.toString());
     }
+
+    // Sauvegarder les modifications via le service
+    Rentals updatedRental = rentalService.updateRental(rental);  // Utiliser directement Rentals
+
+    // Convertir l'entité Rentals mise à jour en RentalsDto
+    RentalsDto updatedRentalDto = new RentalsDto(updatedRental);
+
+    // Retourner le RentalsDto
+    return ResponseEntity.ok(updatedRentalDto);
   }
 
 
+
+
+  /**
+   * Supprime une location par ID
+   */
+  @Operation(summary = "Supprimer une location")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "204", description = "Location supprimée avec succès"),
+    @ApiResponse(responseCode = "404", description = "Location non trouvée")
+  })
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteRental(@PathVariable Integer id) {
     rentalService.deleteRental(id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
+
 }
