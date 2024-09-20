0e454ad0073854c9398ab77d0b3a80ba22757ae1
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-20 15:29:34 +0200

    Expand permitted endpoints in security configuration
    Updated the security configuration to allow public access to Swagger UI and V3 endpoints. This change ensures that documentation and API specification URLs are accessible without authentication.

--- back_end/src/main/java/fr/chatop/projet_3/config/SpringSecurityConfig.java
+++ back_end/src/main/java/fr/chatop/projet_3/config/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig {
     return http.csrf(csrf->csrf.disable())
       .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth->auth
-          .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+          .requestMatchers("/api/auth/register", "/api/auth/login", "/swagger-ui/**", "/v3/**").permitAll()
         .requestMatchers("/uploads/**").permitAll()
         .anyRequest().authenticated())
       .logout(logout ->
