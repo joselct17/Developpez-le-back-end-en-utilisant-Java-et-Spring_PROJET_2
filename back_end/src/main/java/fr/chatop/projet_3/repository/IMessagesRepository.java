9916024db7c1759b201e0df0a2f510a70a4a6b91
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-09-20 15:04:42 +0200

    Add methods to IMessagesRepository for searching by IDs
    Introduced methods to find messages by userId, rentalId, and a combination of both. This enhances the repository's search capabilities and simplifies querying the database for specific message associations.

--- back_end/src/main/java/fr/chatop/projet_3/repository/IMessagesRepository.java
+++ back_end/src/main/java/fr/chatop/projet_3/repository/IMessagesRepository.java
@@ -4,7 +4,19 @@ import fr.chatop.projet_3.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface IMessagesRepository extends JpaRepository<Message, Integer> {
+
+  // Rechercher par userId
+  List<Message> findByUserId(Integer userId);
+
+  // Rechercher par rentalId
+  List<Message> findByRentalId(Integer rentalId);
+
+  // Rechercher par userId et rentalId
+  List<Message> findByUserIdAndRentalId(Integer userId, Integer rentalId);
+
 }
